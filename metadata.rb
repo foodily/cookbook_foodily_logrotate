a6003ff582cb59819e4a29d577dd15bf5b637b69
jeffkimble <foodily@jeffkimble.com>
2013-09-05 15:04:56 -0700

    adding new version and removing pinned logrotate version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Jeff@foodily.com'
 license          'All rights reserved'
 description      'Installs/Configures foodily_logrotate'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
-depends "logrotate", ">= 1.2.2"
+depends 'logrotate'
 
